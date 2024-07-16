ca3985afec6a43a9f25446fbcb8280c6bd3805c0
Petros Papapanagiotou <petros@suprnation.com>
2024-07-16 15:40:35 +0100

    Spring 6 + jakarta namespace

--- wordpress-acf-common-integration/src/main/java/com/suprnation/cms/model/CmsPost.java
+++ wordpress-acf-common-integration/src/main/java/com/suprnation/cms/model/CmsPost.java
@@ -5,10 +5,10 @@ import com.suprnation.cms.marker.CmsPostIdentifier;
 import lombok.*;
 import org.hibernate.annotations.Immutable;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import java.io.Serializable;
 import java.time.ZoneId;
 import java.time.ZoneOffset;
